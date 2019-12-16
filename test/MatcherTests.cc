21912d194a393bde28bfbddc1c1e12cb84235f38
Luboš Krčál <lubos.krcal@gmail.com>
2019-12-16 23:59:32 +1300

    Fix basic matching test

--- test/MatcherTests.cc
+++ test/MatcherTests.cc
@@ -21,22 +21,23 @@ TEST_P(SopangMatcherTest, Match)
 {
     std::pair<std::string, std::string> inParam = GetParam();
     std::ifstream ifs(std::get<0>(inParam), std::ios::binary);
+    ASSERT_TRUE(ifs.good());
     ElasticDegenerateString eds;
     ifs >> eds;
+    ASSERT_TRUE(eds.Size() > 0);
+    ASSERT_TRUE(eds.Segments() > 0);
 
-//    std::string patternFilename = std::get<1>(inParam);
-//    std::ifstream pfs(patternFilename, std::ios::binary);
-//
-//    pfs.seekg(0, std::ios::end);
-//    size_t pattern_size = pfs.tellg();
-//    pfs.seekg(0);
-//
-//    std::string pattern(pattern_size, 0);
-//    pfs.read(&pattern[0], pattern_size);
-//
-    std::string pattern("C");
+    std::ifstream pfs(std::get<1>(inParam), std::ios::binary);
+    ASSERT_TRUE(pfs.good());
+    pfs.seekg(0, std::ios::end);
+    size_t pattern_size = pfs.tellg();
+    pfs.seekg(0);
+
+    std::string pattern(pattern_size, 0);
+    pfs.read(&pattern[0], pattern_size);
 
     std::unordered_set<unsigned> res = SopangMatcher().Match(eds, pattern, "ACGT");
+    std::set<unsigned> sorted_res(std::begin(res), std::end(res));
 
     std::cout << "Match results:";
     for ( auto it = res.begin(); it != res.end(); ++it )
@@ -44,5 +45,5 @@ TEST_P(SopangMatcherTest, Match)
     std::cout << std::endl;
 }
 
-auto values = testing::Values(std::make_pair<std::string,std::string>("data/chr1.eds", "data/pattern8.txt"));
+auto values = testing::Values(std::make_pair<std::string,std::string>("data/chr1337.eds", "data/pattern8.txt"));
 INSTANTIATE_TEST_SUITE_P(SopangMatcherTestSuite, SopangMatcherTest, values);
